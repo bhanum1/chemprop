d58563c97214c09aa63304fbb52f3a1c0061e288
Bhanu <mamillapallibhanu1@gmail.com>
2024-06-12 11:30:44 -0400

    modified:   chemprop/cli/hpopt.py

--- chemprop/cli/hpopt.py
+++ chemprop/cli/hpopt.py
@@ -7,7 +7,6 @@ import sys
 
 from lightning import pytorch as pl
 from lightning.pytorch.callbacks import EarlyStopping
-from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
 import numpy as np
 import torch
 
@@ -263,7 +262,6 @@ def update_args_with_config(args: Namespace, config: dict) -> Namespace:
 def train_model(config, args, train_dset, val_dset, logger, output_transform, input_transforms):
     update_args_with_config(args, config)
 
-    #trainer_logger = TensorBoardLogger(args.hpopt_save_dir, "trainer_logs")
     train_loader = build_dataloader(
         train_dset, args.batch_size, args.num_workers, seed=args.data_seed
     )
@@ -291,7 +289,6 @@ def train_model(config, args, train_dset, val_dset, logger, output_transform, in
         callbacks=[RayTrainReportCallback(), early_stopping],
         plugins=[RayLightningEnvironment()],
         deterministic=args.pytorch_seed is not None,
-        #logger = trainer_logger,
     )
     trainer = prepare_trainer(trainer)
     trainer.fit(model, train_loader, val_loader)
