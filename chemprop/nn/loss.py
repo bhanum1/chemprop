818a83817c77a87cef3d0b30b2f37fcb42da4ce9
Bhanu <mamillapallibhanu1@gmail.com>
2024-06-13 11:11:43 -0400

    modified:   chemprop/nn/loss.py

--- chemprop/nn/loss.py
+++ chemprop/nn/loss.py
@@ -102,8 +102,10 @@ class MSELoss(LossFunction):
         out = lnA + EaR * 1000 * temps
 
         out = out.view(-1,1).float()
-        lnA = lnA.float()
+        lnA = lnA.view(-1,1).float()
+        lnA_targets = lnA_targets.view(-1,1)
         
+        print(lnA.shape, lnA_targets.shape)
 
         lnA_loss = F.mse_loss(lnA, lnA_targets, reduction="none")
         visc_loss = F.mse_loss(out, targets, reduction="none")
