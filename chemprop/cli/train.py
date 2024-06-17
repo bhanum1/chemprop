2bd925852f80f29a5a52572e5c1d2a51c41d300a
bhanum1 <132242829+bhanum1@users.noreply.github.com>
2024-06-17 09:30:14 -0400

    modified:   chemprop/cli/train.py

--- chemprop/cli/train.py
+++ chemprop/cli/train.py
@@ -987,10 +987,11 @@ def main(args):
 
 
         if "regression" in args.task_type:
-            output_scaler = train_dset.normalize_targets()
-            val_dset.normalize_targets(output_scaler)
-            logger.info(f"Train data: mean = {output_scaler.mean_} | std = {output_scaler.scale_}")
-            output_transform = UnscaleTransform.from_standard_scaler(output_scaler)
+            #output_scaler = train_dset.normalize_targets()
+            #val_dset.normalize_targets(output_scaler)
+            #logger.info(f"Train data: mean = {output_scaler.mean_} | std = {output_scaler.scale_}")
+            #output_transform = UnscaleTransform.from_standard_scaler(output_scaler)
+            pass
         else:
             output_transform = None
 
